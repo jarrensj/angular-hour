5ef87c0a46371a29e7f634a60632831a259e6444
Jarren San Jose <sanjose.jarren@gmail.com>
2017-10-02 19:08:22 -0700

    forms

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  name:string;
+  age:number;
+  email:string;
+  address:Address;
+  hobbies:string[];
+  hello:any;
 
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Jarren San Jose';
+    this.email = "sanjose.jarren@gmail.com"
+    this.age = 22;
+    this.address = {
+      street: '123 Street st',
+      city: 'Irvine',
+      state: 'CA'
+    }
+    this.hobbies = ["code", "play basketball"];
+    this.hello = "hello";
+  }
+  onClick(){
+    this.name = "Jarrensj";
+    this.hobbies.push('New Hobby');
+  }
+  addHobby(hobby){
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+  deleteHobby(hobby){
+    for(let i = 0; i < this.hobbies.length; ++i){
+      if(this.hobbies[i] == hobby){
+        this.hobbies.splice(i,1);
+      }
+    }
   }
 
+}
+
+interface Address{
+  street:string,
+  city:string,
+  state:string
 }
